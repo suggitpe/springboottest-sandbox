20c42538816645bae9d66a1ed1ddffeb91b81b86
Peter Suggitt <me@suggs.org.uk>
2018-09-26 09:48:51 +0100

    added cucumber-sping dependency to spice things up a little

--- src/test/java/org/suggs/sandbox/springboottest/greeting/stepdefs/GreetingSteps.java
+++ src/test/java/org/suggs/sandbox/springboottest/greeting/stepdefs/GreetingSteps.java
@@ -1,19 +1,17 @@
 package org.suggs.sandbox.springboottest.greeting.stepdefs;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.restassured.RestAssured;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abiities.CallAnApi;
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.suggs.sandbox.springboottest.ApplicationUnderTest;
 import org.suggs.sandbox.springboottest.greeting.dsl.Say;
 
 import static org.hamcrest.Matchers.is;
@@ -25,6 +23,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class GreetingSteps {
 
+    private static final Logger LOG = LoggerFactory.getLogger(GreetingSteps.class);
     private ConfigurableApplicationContext application;
 
     @LocalServerPort
@@ -34,18 +33,9 @@ public class GreetingSteps {
     public static final String PRODUCER_URL = "http://localhost:";
     private Actor actor;
 
-    @Before
-    public void beforeTests() {
-        application = SpringApplication.run(ApplicationUnderTest.class);
-    }
-
-    @After
-    public void afterTests() {
-        application.stop();
-    }
-
     @Given("^the Greeting Service is running$")
     public void the_producer_is_running() {
+        LOG.info("Checking the producer is running");
         assertThat(localPort, is(not(-1)));
         assertThat(RestAssured.get(PRODUCER_URL + localPort).statusCode(), is(200));
     }
